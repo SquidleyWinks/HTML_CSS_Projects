d559b946a44fcbb8861725c7d7c391a7a7fbe41f
SquidleyWinks <119633090+SquidleyWinks@users.noreply.github.com>
2022-12-19 09:01:52 -0600

    Changes made

---
+++ Basic_HTML_and_Javascript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("This is an alert box (so exciting)");
+
+function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+}
