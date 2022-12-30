e09d9481151b462f6957c4c135c27fe5c4d94b6d
SquidleyWinks <119633090+SquidleyWinks@users.noreply.github.com>
2022-12-30 15:23:43 -0600

    Changes Made

---
+++ New Folder/Hello_World.js
@@ -0,0 +1,8 @@
+var greeting = 'Hello, Wrold!';
+    console.log(greeting);
+
+    var greeting2 = 'Welcome to Node.js';
+    console.log(greeting2)
+
+    var sentence1 = 'Learning how to debug code with the debugger';
+    console.log(sentenceOne);
